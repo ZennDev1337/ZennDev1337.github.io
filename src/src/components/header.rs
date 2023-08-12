21077d06132c8023f301e0a802f649a40dd39e6c
Zenn <zenndev@protonmail.com>
2023-08-12 19:35:44 +0200

    clean up

--- src/src/components/header.rs
+++ src/src/components/header.rs
@@ -5,7 +5,7 @@ pub fn Header() -> Html {
     html! {
         <section class="section">
           <h1 id="hacker-title" class="title is-italic ">
-          {"ZennDev1337 aka Zenny🦀"}
+          {"ZennDev1337 aka Zenny 🦀"}
           </h1>
           <h2 id="hacker-subtitle" class="subtitle ">
             {"A Backend Developer"}
